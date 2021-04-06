ac397bc764161e545ffc81f74793d226176364ce
yokoko <yoko.syun.1408@gmail.com>
2021-04-07 08:01:16 +0900

    fist commit

--- train.sh
+++ train.sh
@@ -1,8 +1,1 @@
-python3 train.py \
---model PackNet01 \
---maxdisp 192 \
---datapath /media/yoko/SSD-PGU3/workspace/datasets/KITTI/monodepth/KITTI_tiny/ \
---epochs 300 \
---batch_size 1 \
---savemodel ./result \
---seed 1
+sudo python3 train.py configs/train_kitti.yaml
